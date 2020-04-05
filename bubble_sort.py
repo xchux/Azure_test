2a94cc5dc0fee3d46213320f778b814caf58bf56
Chu(朱恆慶 <chicochu9333@gmail.com>
2020-04-05 14:21:03 +0800

    Add bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(collection):
+    length = len(collection)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                swapped = True
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+        if not swapped:
+            break  # Stop iteration if the collection is sorted.
+    return collection
